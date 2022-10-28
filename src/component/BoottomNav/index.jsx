27cfe99db000c027009e32bb2a922544adb9605f
tony <201267151@qq.com>
2022-10-28 23:29:05 +0800

    init

---
+++ src/component/BoottomNav/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BottomNav = ()=>{
+    return(
+        <div className='nav-footer'>
+            <ul className='clear-fix'>
+                <li>
+                    <i className='iconfont icon-home'></i>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default BottomNav
